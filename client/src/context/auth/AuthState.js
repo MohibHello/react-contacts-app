98b2004730025f19cf44426c71eaec842f53700a
Mohib <www.muhibkhan@gmail.com>
2020-05-11 23:24:42 +0530

    User Register using mongoDB

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -9,8 +9,10 @@ import {
     LOGIN_SUCCESS,
     LOGIN_FAIL,
     LOGOUT,
-    CLEAR_ERRORS
+    CLEAR_ERRORS,
+    CLEAR_CURRENT
 } from "../types";
+import axios from "axios"
 
 const AuthState = (props) => {
   const initialState = {
@@ -24,14 +26,41 @@ const AuthState = (props) => {
   const [state, dispatch] = useReducer(AuthReducer, initialState);
 
  // Load User
+const loadUser = () =>console.log('loaduser');
+
 
  // Register User
+  const register =async formData =>{
+    const config ={
+      headers:{
+        'Content-Type':'application/json'
+      }
+    }
+    try {
+      const res=await axios.post('api/users',formData,config);
+
+      //res.data will be token
+      dispatch({type: REGISTER_SUCCESS, payload:res.data})
+
+    }catch(err){
+      console.log(err)
+      dispatch({type: REGISTER_FAIL, payload:err.response.data.msg})
+    }
+
+
+
+  }
+
+
 
  //Login User
+ const login = () =>console.log('loaduser');
 
  //Logout
+const logout = () =>console.log('loaduser');
 
  //Clear Errors
+ const clearErrors = () =>dispatch({type:CLEAR_ERRORS})
 
 
 
@@ -43,6 +72,11 @@ const AuthState = (props) => {
        isAuthenticated:state.isAuthenticated,
        user:state.user,
        error:state.error,
+       loadUser,
+       register,
+       login,
+       logout,
+       clearErrors
       }}
     >
       {props.children}
